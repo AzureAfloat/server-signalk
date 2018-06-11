89c8b5ca0149a7c9e42df43f25081fdb5cdeef6e
Jeremy Foster <jeremy.foster@live.com>
2018-06-11 14:52:16 -0700

    further simplify index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,4 @@
-// import * as SignalKServer from "signalk-server";
-var SignalKServer = require("signalk-server");
+import SignalKServer from "signalk-server";
 
 require('dotenv').config();
-let server = new SignalKServer();
-
-server.start()
-  .catch(err => {
-    console.log(err)
-    process.exit(-1)
-  })
-
-//make sure we're connected to the web
-
+new SignalKServer().start();
